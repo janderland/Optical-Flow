ae3d2d99ff630a419c1b1c6d51b0ff65bb9f8766
Jon Anderson <littlejon@troy.local>
2014-10-08 10:18:39 -0700

    Initial Commit

---
+++ OpticalFlow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpticalFlow
+//
+//  Created by Jon Anderson on 10/8/14.
+//  Copyright (c) 2014 Jon Anderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
